fd2ca603c0ec873b98d26fb123f99f3e01a49b7a
Phillip Bryan <bryanpc01.ceal@gmail.com>
2018-02-20 11:34:25 -0500

    Version1

---
+++ mainScript.js
@@ -0,0 +1,219 @@
+ // SLIDE 1
+
+let FreqBtn1 = document.getElementById('Fbtn1');
+let BandBtn1 = document.getElementById('Bbtn1');
+
+document.getElementById('aFreqAnswer').addEventListener('click',function(){
+    FreqBtn1.classList.replace('btn-danger', 'btn-success')
+    FreqBtn1.classList.replace('btn-outline-secondary', 'btn-success')
+    FreqBtn1.innerHTML = "Frequency: 5Ghz"
+})
+
+document.getElementById('aBandAnswer').addEventListener('click',function(){
+    BandBtn1.classList.replace('btn-danger', 'btn-success')
+    BandBtn1.classList.replace('btn-outline-secondary', 'btn-success')
+    BandBtn1.innerHTML = "Bandwidth: 54Mbps"
+})
+
+document.getElementById('wf1').addEventListener('click',function(){
+    FreqBtn1.classList.replace('btn-success', 'btn-danger')
+    FreqBtn1.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn1.innerHTML = "Frequency: Try Again"
+})
+document.getElementById('wf2').addEventListener('click',function(){
+    FreqBtn1.classList.replace('btn-success', 'btn-danger')
+    FreqBtn1.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn1.innerHTML = "Frequency: Try Again"
+})
+
+document.getElementById('wb1').addEventListener('click',function(){
+    BandBtn1.classList.replace('btn-success', 'btn-danger')
+    BandBtn1.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn1.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb2').addEventListener('click',function(){
+    BandBtn1.classList.replace('btn-success', 'btn-danger')
+    BandBtn1.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn1.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb3').addEventListener('click',function(){
+    BandBtn1.classList.replace('btn-success', 'btn-danger')
+    BandBtn1.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn1.innerHTML = "Bandwidth: Try Again"
+})
+
+// SLIDE 2
+
+let FreqBtn2 = document.getElementById('Fbtn2');
+let BandBtn2 = document.getElementById('Bbtn2');
+
+document.getElementById('bFreqAnswer').addEventListener('click',function(){
+    FreqBtn2.classList.replace('btn-danger', 'btn-success')
+    FreqBtn2.classList.replace('btn-outline-secondary', 'btn-success')
+    FreqBtn2.innerHTML = "Frequency: 2.4Ghz"
+})
+
+document.getElementById('bBandAnswer').addEventListener('click',function(){
+    BandBtn2.classList.replace('btn-danger', 'btn-success')
+    BandBtn2.classList.replace('btn-outline-secondary', 'btn-success')
+    BandBtn2.innerHTML = "Bandwidth: 11Mbps"
+})
+
+document.getElementById('wf1.2').addEventListener('click',function(){
+    FreqBtn2.classList.replace('btn-success', 'btn-danger')
+    FreqBtn2.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn2.innerHTML = "Frequency: Try Again"
+})
+document.getElementById('wf2.2').addEventListener('click',function(){
+    FreqBtn2.classList.replace('btn-success', 'btn-danger')
+    FreqBtn2.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn2.innerHTML = "Frequency: Try Again"
+})
+
+document.getElementById('wb1.2').addEventListener('click',function(){
+    BandBtn2.classList.replace('btn-success', 'btn-danger')
+    BandBtn2.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn2.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb2.2').addEventListener('click',function(){
+    BandBtn2.classList.replace('btn-success', 'btn-danger')
+    BandBtn2.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn2.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb3.2').addEventListener('click',function(){
+    BandBtn2.classList.replace('btn-success', 'btn-danger')
+    BandBtn2.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn2.innerHTML = "Bandwidth: Try Again"
+})
+
+// SLIDE 3
+
+let FreqBtn3 = document.getElementById('Fbtn3');
+let BandBtn3 = document.getElementById('Bbtn3');
+
+document.getElementById('gFreqAnswer').addEventListener('click',function(){
+    FreqBtn3.classList.replace('btn-danger', 'btn-success')
+    FreqBtn3.classList.replace('btn-outline-secondary', 'btn-success')
+    FreqBtn3.innerHTML = "Frequency: 2.4Ghz"
+})
+
+document.getElementById('gBandAnswer').addEventListener('click',function(){
+    BandBtn3.classList.replace('btn-danger', 'btn-success')
+    BandBtn3.classList.replace('btn-outline-secondary', 'btn-success')
+    BandBtn3.innerHTML = "Bandwidth: 54Mbps"
+})
+
+document.getElementById('wf1.3').addEventListener('click',function(){
+    FreqBtn3.classList.replace('btn-success', 'btn-danger')
+    FreqBtn3.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn3.innerHTML = "Frequency: Try Again"
+})
+document.getElementById('wf2.3').addEventListener('click',function(){
+    FreqBtn3.classList.replace('btn-success', 'btn-danger')
+    FreqBtn3.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn3.innerHTML = "Frequency: Try Again"
+})
+
+document.getElementById('wb1.3').addEventListener('click',function(){
+    BandBtn3.classList.replace('btn-success', 'btn-danger')
+    BandBtn3.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn3.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb2.3').addEventListener('click',function(){
+    BandBtn3.classList.replace('btn-success', 'btn-danger')
+    BandBtn3.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn3.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb3.3').addEventListener('click',function(){
+    BandBtn3.classList.replace('btn-success', 'btn-danger')
+    BandBtn3.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn3.innerHTML = "Bandwidth: Try Again"
+})
+
+// SLIDE 4
+
+let FreqBtn4 = document.getElementById('Fbtn4');
+let BandBtn4 = document.getElementById('Bbtn4');
+
+document.getElementById('nFreqAnswer').addEventListener('click',function(){
+    FreqBtn4.classList.replace('btn-danger', 'btn-success')
+    FreqBtn4.classList.replace('btn-outline-secondary', 'btn-success')
+    FreqBtn4.innerHTML = "Frequency: 2.4Ghz & 5Ghz"
+})
+
+document.getElementById('nBandAnswer').addEventListener('click',function(){
+    BandBtn4.classList.replace('btn-danger', 'btn-success')
+    BandBtn4.classList.replace('btn-outline-secondary', 'btn-success')
+    BandBtn4.innerHTML = "Bandwidth: 600Mbps"
+})
+
+document.getElementById('wf1.4').addEventListener('click',function(){
+    FreqBtn4.classList.replace('btn-success', 'btn-danger')
+    FreqBtn4.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn4.innerHTML = "Frequency: Try Again"
+})
+document.getElementById('wf2.4').addEventListener('click',function(){
+    FreqBtn4.classList.replace('btn-success', 'btn-danger')
+    FreqBtn4.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn4.innerHTML = "Frequency: Try Again"
+})
+
+document.getElementById('wb1.4').addEventListener('click',function(){
+    BandBtn4.classList.replace('btn-success', 'btn-danger')
+    BandBtn4.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn4.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb2.4').addEventListener('click',function(){
+    BandBtn4.classList.replace('btn-success', 'btn-danger')
+    BandBtn4.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn4.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb3.4').addEventListener('click',function(){
+    BandBtn4.classList.replace('btn-success', 'btn-danger')
+    BandBtn4.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn4.innerHTML = "Bandwidth: Try Again"
+})
+
+// SLIDE 5
+
+let FreqBtn5 = document.getElementById('Fbtn5');
+let BandBtn5 = document.getElementById('Bbtn5');
+
+document.getElementById('acFreqAnswer').addEventListener('click',function(){
+    FreqBtn5.classList.replace('btn-danger', 'btn-success')
+    FreqBtn5.classList.replace('btn-outline-secondary', 'btn-success')
+    FreqBtn5.innerHTML = "Frequency: 5Ghz"
+})
+
+document.getElementById('acBandAnswer').addEventListener('click',function(){
+    BandBtn5.classList.replace('btn-danger', 'btn-success')
+    BandBtn5.classList.replace('btn-outline-secondary', 'btn-success')
+    BandBtn5.innerHTML = "Bandwidth: Gigabit Speed"
+})
+
+document.getElementById('wf1.5').addEventListener('click',function(){
+    FreqBtn5.classList.replace('btn-success', 'btn-danger')
+    FreqBtn5.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn5.innerHTML = "Frequency: Try Again"
+})
+document.getElementById('wf2.5').addEventListener('click',function(){
+    FreqBtn5.classList.replace('btn-success', 'btn-danger')
+    FreqBtn5.classList.replace('btn-outline-secondary', 'btn-danger')
+    FreqBtn5.innerHTML = "Frequency: Try Again"
+})
+
+document.getElementById('wb1.5').addEventListener('click',function(){
+    BandBtn5.classList.replace('btn-success', 'btn-danger')
+    BandBtn5.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn5.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb2.5').addEventListener('click',function(){
+    BandBtn5.classList.replace('btn-success', 'btn-danger')
+    BandBtn5.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn5.innerHTML = "Bandwidth: Try Again"
+})
+document.getElementById('wb3.5').addEventListener('click',function(){
+    BandBtn5.classList.replace('btn-success', 'btn-danger')
+    BandBtn5.classList.replace('btn-outline-secondary', 'btn-danger')
+    BandBtn5.innerHTML = "Bandwidth: Try Again"
+})
